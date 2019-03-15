60eb3e9d45eae52eff6d5975d68061aa371ab30c
FZarattini <felipezarattini02@hotmail.com>
2019-03-15 13:13:32 -0300

    Ship sway effect

---
+++ PirateShip/Assets/Camera/CameraSway.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSway : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam;
+    bool goUp;
+    public float upSwayLimit;
+    public float downSwayLimit;
+    public float swayFactor;
+
+    private void Start()
+    {
+        goUp = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (goUp)
+        {
+            upSway();
+        }
+        else
+        {
+            downSway();
+        }
+
+        /*if (vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y >= 0.6)
+        {
+            Debug.Log("deveria descer!");
+            downSway();
+        }
+
+        if(vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y <= 0.6)
+        {
+            Debug.Log("Deveria subir!");
+            upSway();
+        }*/
+
+    }
+
+    private void upSway()
+    {
+        vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y += 0.1f * Time.deltaTime * swayFactor;
+        if(vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y >= upSwayLimit)
+        {
+            goUp = false;
+        }
+    }
+
+    private void downSway()
+    {
+        vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y -= 0.1f * Time.deltaTime * swayFactor;
+        if(vcam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y <= downSwayLimit)
+        {
+            goUp = true;
+        }
+    }
+}
