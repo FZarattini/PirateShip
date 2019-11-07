b7cd8aec16b87b901f5b50475d76fc952dc42c16
FZarattini <felipezarattini02@hotmail.com>
2019-11-07 14:39:13 -0200

    Empathy

---
+++ PirateShip/Assets/Scripts/AI/Empathy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EmpathyEnum
+{
+    sympathetic,
+    comprehensive,
+    neutral,
+    incomprehensive,
+    hateful
+};
+
+public class Empathy : MonoBehaviour
+{
+
+    public EmpathyEnum empathy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CalculateEmpathy()
+    {
+
+    }
+
+}
--- PirateShip/Assets/Scripts/Player/PlayerController.cs
+++ PirateShip/Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+    public Personality personality;
 
     // Start is called before the first frame update
     void Start()
