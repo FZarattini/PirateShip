22898ab112de957dc001efe28bcae9980a474ea0
FZarattini <felipezarattini02@hotmail.com>
2020-01-18 20:16:47 -0200

    Animação MinerBoss

---
+++ PirateShip/Assets/Scripts/Misc/TimelineManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+    public PlayerMovement playerMov;
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+        playerMov.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(director.state != PlayState.Playing && !fix)
+        {
+            fix = true;
+            playerMov.enabled = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+        }
+    }
+}
