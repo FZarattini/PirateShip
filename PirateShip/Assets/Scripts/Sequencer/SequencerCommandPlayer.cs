b29e7cba8995ac129c6c7e643bc7de5e8721be08
FZarattini <felipezarattini02@hotmail.com>
2020-01-30 13:20:39 -0200

    Continue Fix

---
+++ PirateShip/Assets/Scripts/Sequencer/SequencerCommandPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PixelCrushers.DialogueSystem.SequencerCommands
+{
+    public class SequencerCommandPlayer : SequencerCommand
+    {
+
+        bool playerState;
+        GameObject player;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerState = GetParameterAsBool(0);
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playerState == false)
+            {
+                player.GetComponent<PlayerMovement>().enabled = false;
+            }
+            else
+            {
+                player.GetComponent<PlayerMovement>().enabled = true;
+                Stop();
+            }
+        }
+    }
+}
