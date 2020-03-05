6480d8b0e20a1c65b92aa52c26143ab9c32e6a3d
FZarattini <felipezarattini02@hotmail.com>
2020-03-05 14:26:15 -0300

    Scenery

---
+++ PirateShip/Assets/Scripts/AI/Trackers/Openness.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openness : MonoBehaviour
+{
+
+    PlayerController player;
+    Inventory inventory;
+
+
+    Collider2D areas;
+    int visitedAreas = 0;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
